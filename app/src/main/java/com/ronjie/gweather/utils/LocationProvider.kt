ca5ab96f46a2f414c628da03335247b7033fc9df
itsmeRonjie <itsme.Ronjie@gmail.com>
2025-09-20 11:55:41 +0800

    Add LocationProvider utility
    This commit introduces `LocationProvider.kt`, a utility class for handling location updates.
    
    Key features:
    - Uses `FusedLocationProviderClient` for location updates.
    - Exposes location updates as a Kotlin Flow (`locationUpdates`).
    - Handles location permission requests using `ActivityResultLauncher`.
    - Provides methods to check for and request location permissions.
    - Starts location updates internally after permission is granted.
    - Includes logic to stop location updates when the flow is closed.
    - Defines a data class `MyLatLong` for latitude and longitude.

---
+++ app/src/main/java/com/ronjie/gweather/utils/LocationProvider.kt
@@ -0,0 +1,110 @@
+package com.ronjie.gweather.utils
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Looper
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.ronjie.gweather.domain.model.MyLatLong
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+class LocationProvider(private val activity: ComponentActivity) {
+    private val fusedLocationProviderClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(activity)
+
+    private lateinit var locationCallback: LocationCallback
+
+    val locationUpdates: Flow<MyLatLong> = callbackFlow {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.lastLocation?.let { location ->
+                    trySend(MyLatLong(lat = location.latitude, long = location.longitude))
+                }
+            }
+        }
+
+        if (hasLocationPermission()) startLocationUpdatesInternal()
+
+        awaitClose { stopLocationUpdates() }
+    }
+
+    private val locationPermissionLauncher: ActivityResultLauncher<Array<String>> =
+        activity.registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true ||
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+            ) {
+                if (::locationCallback.isInitialized) {
+                    startLocationUpdatesInternal()
+                }
+                Toast.makeText(
+                    activity.applicationContext,
+                    "Location permission granted",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    activity.applicationContext,
+                    "Location permission denied",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    fun hasLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            activity,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    activity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun requestLocationPermissions() {
+        locationPermissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdatesInternal() {
+        if (!::locationCallback.isInitialized) return
+
+        val locationRequest = LocationRequest
+            .Builder(Priority.PRIORITY_HIGH_ACCURACY, 100)
+            .setWaitForAccurateLocation(false)
+            .setMinUpdateIntervalMillis(3000)
+            .setMaxUpdateDelayMillis(100)
+            .build()
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
+    private fun stopLocationUpdates() {
+        if (::locationCallback.isInitialized) {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+}
